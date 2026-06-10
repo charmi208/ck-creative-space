940203572033f9c85e0a13539ff3156ad3cb102d
itdc-Raj <rajbhatt.idc@silverakuni.ac.in>
2026-06-10 23:26:44 +0530

    changes in config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  output: 'export',
+
   images: {
+    unoptimized: true,
     formats: ['image/avif', 'image/webp'],
   },
+
+  basePath: '/ck-creative-space',
+  assetPrefix: '/ck-creative-space/',
 };
+
 export default nextConfig;
